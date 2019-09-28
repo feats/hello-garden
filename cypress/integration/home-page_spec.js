cc8ef53358c5fc038bbf959b8f2898b6e95ce106
Jens Peter Secher <jpsecher@gmail.com>
2019-09-28 21:49:41 +0200

    Button click.

--- cypress/integration/home-page_spec.js
+++ cypress/integration/home-page_spec.js
@@ -1,17 +1,22 @@
 /* eslint-env mocha */
 /* global cy */
 
-describe('Landing page', () => {
+describe('Web app', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('heading', () => {
+  it('landing page', () => {
     cy.get('h1')
       .should('have.text', 'The Oracle')
+    cy.get('p')
+      .should('have.text', 'Do you want to know the meaning of everything?')
   })
 
-  it('answer', () => {
+  it('question', () => {
+    cy.get('button')
+      .contains('Please tell me')
+      .click()
     cy.get('p')
       .contains(/The answer is [0-9]+/)
   })
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,8 +5,26 @@ const express = require('express')
 const frontend = express()
 
 frontend.get('/', (req, res) => {
-  const number = 42
-  res.send(`<html><body><h1>The Oracle</h1><p>The answer is ${number}</p></body></html>`)
+  res.send(`<!DOCTYPE html><html>
+    <head>
+      <script type="text/javascript"
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
+      </script>
+      <script type="text/javascript">
+        $(document).ready(() => {
+          $("#get-answer").click(() => {
+            $("#contents").replaceWith("<p>The answer is 42<p>")
+          })
+        })
+      </script>
+    </head>
+    <body>
+      <h1>The Oracle</h1>
+      <div id="contents">
+        <p>Do you want to know the meaning of everything?</p>
+        <button id="get-answer">Please tell me</button>
+       </div>
+    </body></html>`)
 })
 
 module.exports = frontend
