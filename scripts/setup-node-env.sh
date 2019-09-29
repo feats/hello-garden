7e5ed2dbe4252689231484c6c92266f00028708d
Jens Peter Secher <jpsecher@gmail.com>
2019-09-29 12:38:38 +0200

    lib -> shared.

--- scripts/setup-node-env.sh
+++ scripts/setup-node-env.sh
@@ -7,7 +7,7 @@ log_() { echo "[$ME] $@"; }
 log_ "--> Setting up symbolic links"
 mkdir -p node_modules
 cd node_modules
-[ -L __ ] || ln -s ../src/lib __
+[ -L __ ] || ln -s ../src/shared __
 [ -L backend ] || ln -s ../src/backend backend
 [ -L frontend ] || ln -s ../src/frontend frontend
 [ -L fixtures ] || ln -s ../src/fixtures fixtures
